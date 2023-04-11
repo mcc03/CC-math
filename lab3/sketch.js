04fff0a3685c35a4dc93839427ecfc74f8f5168d
N00212147 <N00212147@iadt.ie>
2023-04-11 13:19:17 +0100

    lab3 q2

--- lab3/sketch.js
+++ lab3/sketch.js
@@ -2,10 +2,25 @@ let ball = []
 
 
 function setup(){
+
+    // Q2 code
+    // angle the ball will travel at
+	let angle = 45;
+
+	// converting angle to radians to use for trig
+	let radians = angle * Math.PI / 180;
+
+	// speed
+	let speed = 5;
+
+	// calculating the x and y velocities
+	let xv = speed * Math.cos(radians);
+	let yv = speed * Math.sin(radians);
+
     createCanvas(500, 500);
     angleMode(DEGREES);
 
-    ball.push(new Ball(2, 3, 2, 3, color(255,100,0)));
+    ball.push(new Ball(2, 3, xv, yv, color(255,100,0)));
 }
 
 function draw(){
